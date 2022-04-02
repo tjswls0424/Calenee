7b9edde5f96232775b9f7101807777d3880a2934
tjswls0424 <dltjswls0424@gmail.com>
2022-04-03 01:04:57 +0900

    [add] set fragment (calendar, memo, todo list)

--- app/src/main/java/org/jin/calenee/MainActivity.kt
+++ app/src/main/java/org/jin/calenee/MainActivity.kt
@@ -2,10 +2,38 @@ package org.jin.calenee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import org.jin.calenee.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+
+        supportFragmentManager.beginTransaction()
+            .add(binding.mainFrameLayout.id, CalendarFragment()).commit()
+
+        binding.mainBottomNavigationView.setOnItemSelectedListener { item ->
+            replaceFragment(
+                when(item.itemId) {
+                    R.id.calendar -> CalendarFragment()
+                    R.id.memo -> MemoFragment()
+                    else -> TodoFragment()
+                }
+            )
+
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(binding.mainFrameLayout.id, fragment).commit()
     }
 }
