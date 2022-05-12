3d4561b90404f6e59667d323f2bc238180c9b524
tjswls0424 <dltjswls0424@gmail.com>
2022-05-12 13:55:49 +0900

    [delete] User model, User dao

--- app/src/main/java/org/jin/calenee/database/AppDatabase.kt
+++ app/src/main/java/org/jin/calenee/database/AppDatabase.kt
@@ -5,26 +5,23 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import org.jin.calenee.database.dao.CalendarDao
-import org.jin.calenee.database.dao.UserDao
 import org.jin.calenee.database.model.Calendar
-import org.jin.calenee.database.model.User
 
-@Database(entities = [User::class, Calendar::class], version = 1, exportSchema = false)
+@Database(entities = [Calendar::class], version = 1, exportSchema = false)
 abstract class AppDatabase: RoomDatabase() {
-    abstract fun userDao(): UserDao
     abstract fun calendarDao(): CalendarDao
 
     companion object {
         private var instance: AppDatabase? = null
 
         @Synchronized
-        fun getInstance(context: Context): AppDatabase? {
+        fun getInstance(context: Context, dbName: String = "none"): AppDatabase? {
             if (instance == null) {
                 synchronized(AppDatabase::class) {
                     instance = Room.databaseBuilder(
                         context.applicationContext,
                         AppDatabase::class.java,
-                        "user-database"
+                        dbName
                     ).build()
                 }
             }
