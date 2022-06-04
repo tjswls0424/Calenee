04f87616018dad2fac7041db61b799ede9d1a159
tjswls0424 <dltjswls0424@gmail.com>
2022-06-04 19:48:30 +0900

    [fix] delete and edit unnecessary parts

--- app/src/main/java/org/jin/calenee/DiaryFragment.kt
+++ app/src/main/java/org/jin/calenee/DiaryFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [TodoFragment.newInstance] factory method to
+ * Use the [MemoFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class TodoFragment : Fragment(R.layout.fragment_todo) {
+class DiaryFragment : Fragment(R.layout.fragment_diary) {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class TodoFragment : Fragment(R.layout.fragment_todo) {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_todo, container, false)
+        return inflater.inflate(R.layout.fragment_diary, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class TodoFragment : Fragment(R.layout.fragment_todo) {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment TodoFragment.
+         * @return A new instance of fragment MemoFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            TodoFragment().apply {
+            DiaryFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/org/jin/calenee/MenuFragment.kt
+++ app/src/main/java/org/jin/calenee/MenuFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MemoFragment.newInstance] factory method to
+ * Use the [TodoFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MemoFragment : Fragment(R.layout.fragment_memo) {
+class MenuFragment : Fragment(R.layout.fragment_menu) {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class MemoFragment : Fragment(R.layout.fragment_memo) {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_memo, container, false)
+        return inflater.inflate(R.layout.fragment_menu, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class MemoFragment : Fragment(R.layout.fragment_memo) {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MemoFragment.
+         * @return A new instance of fragment TodoFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MemoFragment().apply {
+            MenuFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
