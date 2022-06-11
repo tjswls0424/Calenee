2364efb3fc9c1c7a4ae36d93be9c997f4ab88ef7
tjswls0424 <dltjswls0424@gmail.com>
2022-06-12 06:43:51 +0900

    [add] profile input layout (couple info input)

--- app/src/main/java/org/jin/calenee/ConnectionInputActivity.kt
+++ app/src/main/java/org/jin/calenee/ConnectionInputActivity.kt
@@ -2,10 +2,16 @@ package org.jin.calenee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jin.calenee.databinding.ActivityConnectionInputBinding
 
 class ConnectionInputActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityConnectionInputBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_connection_input)
+
+        setContentView(binding.root)
     }
 }
