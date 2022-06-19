00f63067f05bcd70cefa12274f8bec16fcf2a870
tjswls0424 <dltjswls0424@gmail.com>
2022-06-19 14:48:29 +0900

    [fix] fix persistent BottomSheet not working properly

--- app/src/main/java/org/jin/calenee/chat/ChattingActivity.kt
+++ app/src/main/java/org/jin/calenee/chat/ChattingActivity.kt
@@ -2,9 +2,8 @@ package org.jin.calenee.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetDialog
-import org.jin.calenee.R
 import org.jin.calenee.databinding.ActivityChattingBinding
 
 class ChattingActivity : AppCompatActivity() {
@@ -13,46 +12,42 @@ class ChattingActivity : AppCompatActivity() {
         ActivityChattingBinding.inflate(layoutInflater)
     }
 
-    private val bottomSheetDialog by lazy {
-        setBottomSheetDialog()
+    private val bottomSheetBehavior by lazy {
+        BottomSheetBehavior.from(binding.bottomSheetView)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         listener()
-
-
         setContentView(binding.root)
     }
 
     private fun listener() {
         binding.plusBtn.setOnClickListener {
-            window.statusBarColor = getColor(R.color.transparent)
-            bottomSheetDialog.apply {
-                behavior.state = BottomSheetBehavior.STATE_COLLAPSED
-            }.show()
+            if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED) {
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                binding.coordinatorLayout.visibility = View.GONE
+            } else {
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+                binding.coordinatorLayout.visibility = View.VISIBLE
+            }
         }
-    }
 
-    private fun setBottomSheetDialog(): BottomSheetDialog {
-        val bottomSheetView = layoutInflater.inflate(R.layout.chat_botom_sheet_layout, null)
-
-        return BottomSheetDialog(this, R.style.BottomSheetDialog).apply {
-            setContentView(bottomSheetView)
-
-            behavior.state = BottomSheetBehavior.STATE_HIDDEN
-
+        binding.view.setOnClickListener {
+            if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED) {
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                binding.coordinatorLayout.visibility = View.GONE
+            }
         }
     }
 
     override fun onBackPressed() {
-        super.onBackPressed()
-
-        if (bottomSheetDialog.isShowing) {
-            bottomSheetDialog.dismiss()
-//            bottomSheetDialog.behavior.state = BottomSheetBehavior.STATE_HIDDEN
+        if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED) {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+            binding.coordinatorLayout.visibility = View.GONE
+        } else {
+            super.onBackPressed()
         }
     }
 }
