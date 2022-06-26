f0cb78c12ec85d3909e16da29aba817fbbb170fa
tjswls0424 <dltjswls0424@gmail.com>
2022-06-26 17:03:12 +0900

    [fix] bottom sheet and soft input(keyboard) switching

--- app/src/main/java/org/jin/calenee/chat/ChattingActivity.kt
+++ app/src/main/java/org/jin/calenee/chat/ChattingActivity.kt
@@ -1,8 +1,13 @@
 package org.jin.calenee.chat
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import org.jin.calenee.databinding.ActivityChattingBinding
 
@@ -16,36 +21,86 @@ class ChattingActivity : AppCompatActivity() {
         BottomSheetBehavior.from(binding.bottomSheetView)
     }
 
+    private var isKeyboardShown: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+        getSoftInputHeight()
         listener()
         setContentView(binding.root)
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private fun listener() {
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        var viewHeight = -1
+
+        binding.root.viewTreeObserver.addOnGlobalLayoutListener {
+            val rect = Rect()
+            binding.root.getWindowVisibleDisplayFrame(rect)
+            val screenHeight = binding.root.rootView.height
+            val keyBoardHeight = screenHeight - rect.bottom
+            if (keyBoardHeight > screenHeight * 0.15) {
+                if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_COLLAPSED) {
+                    // bottom sheet OFF -> soft keyboard appear
+                    setLottieInitialState()
+                    bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                    binding.coordinatorLayout.visibility = View.GONE
+
+                    inputMethodManager.showSoftInput(binding.root, 0)
+                }
+                Log.d("k_test", "is showing")
+            } else {
+                Log.d("k_test", "is closed")
+            }
+
+            val currentViewHeight = binding.root.height
+            if (currentViewHeight > viewHeight) {
+                binding.bottomSheetView.minimumHeight = currentViewHeight/2 - binding.messageEt.height
+                bottomSheetBehavior.peekHeight = currentViewHeight/2 - binding.messageEt.height
+
+                viewHeight = currentViewHeight
+            }
+        }
+
         binding.lottieAddCloseBtn.setOnClickListener {
-            if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED) {
+            if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_COLLAPSED) {
+                // bottom sheet OFF -> soft keyboard appear
                 setLottieInitialState()
                 bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
                 binding.coordinatorLayout.visibility = View.GONE
+
+                inputMethodManager.showSoftInput(binding.root, 0)
             } else {
                 binding.lottieAddCloseBtn.apply {
                     progress = 0.0f
                     playAnimation()
                 }
-                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
                 binding.coordinatorLayout.visibility = View.VISIBLE
+
+                inputMethodManager.hideSoftInputFromWindow(binding.root.windowToken, 0)
             }
+
         }
 
         binding.view.setOnClickListener {
-            if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED) {
+            if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_COLLAPSED) {
                 setLottieInitialState()
                 bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
                 binding.coordinatorLayout.visibility = View.GONE
             }
         }
+    }
+
+    private fun getSoftInputHeight() {
+
+
+
+
+
     }
 
     private fun setLottieInitialState() {
@@ -56,7 +111,7 @@ class ChattingActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
-        if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED) {
+        if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_COLLAPSED) {
             setLottieInitialState()
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
             binding.coordinatorLayout.visibility = View.GONE
