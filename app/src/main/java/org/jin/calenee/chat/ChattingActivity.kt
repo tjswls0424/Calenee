752f538aeb207153c5141d3eac2df1cbfce058a0
tjswls0424 <dltjswls0424@gmail.com>
2022-07-07 16:22:06 +0900

    [fix] fixed an issue where nickname could not be acquired when activity first running

--- app/src/main/java/org/jin/calenee/chat/ChattingActivity.kt
+++ app/src/main/java/org/jin/calenee/chat/ChattingActivity.kt
@@ -3,13 +3,13 @@ package org.jin.calenee.chat
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.graphics.Rect
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.firebase.auth.FirebaseAuth
@@ -17,7 +17,6 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import org.jin.calenee.databinding.ActivityChattingBinding
-import java.lang.RuntimeException
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -46,6 +45,7 @@ class ChattingActivity : AppCompatActivity() {
     private var isKeyboardShown: Boolean = false
     private var chatDataList: MutableList<ChatData> = mutableListOf()
     private var message: String = ""
+    private var nickname: String = ""
 
 //    override fun onStart() {
 //        super.onStart()
@@ -57,8 +57,9 @@ class ChattingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        listener()
         setContentView(binding.root)
+        listener()
+        getNickname()
     }
 
     private fun initRecycler() {
@@ -130,12 +131,7 @@ class ChattingActivity : AppCompatActivity() {
 
         binding.sendBtn.setOnClickListener {
             if (message.isNotEmpty()) {
-                ChatData(
-                    nickname = getNickname(),
-                    message = message,
-                    time = getCurrentTimeStamp(TIME),
-                    viewType = 1
-                ).also {
+                ChatData(nickname, message, getCurrentTimeStamp(TIME), 1).also {
                     chatDataList.add(it)
                     saveChatData(it)
                 }
@@ -161,27 +157,26 @@ class ChattingActivity : AppCompatActivity() {
         })
     }
 
-    private fun getNickname(): String {
-        var nickname = ""
-        firestore.collection("user").document(currentUserEmail)
+    // 첫 실행시 (함수 종료 될 때까지) success listener에 값이 들어오기까지 몇 초 delay가 있기 때문에 미리 호출
+    private fun getNickname() {
+        firestore.collection("user")
+            .document(currentUserEmail)
             .get()
             .addOnSuccessListener { doc ->
-                Log.d("db_test/login-doc", doc.data.toString())
                 nickname = doc.data?.get("nickname").toString()
+                Log.d("db_test/nickname1", nickname)
             }
             .addOnFailureListener {
                 Log.d("db_test/login-err", "${it.printStackTrace()}")
             }
-
-        return nickname
     }
 
     private fun saveChatData(data: ChatData) {
         val path = getCurrentTimeStamp(DATE_TIME)
         val myRef = FirebaseDatabase.getInstance().getReference(path)
         myRef.apply {
-            child("senderNickname").setValue(data.nickname)
             child("senderEmail").setValue(currentUserEmail)
+            child("senderNickname").setValue(data.nickname)
             child("message").setValue(data.message)
             child("createdAt").setValue(data.time)
             child("read").setValue(false)
@@ -197,7 +192,7 @@ class ChattingActivity : AppCompatActivity() {
 
     private fun getCurrentTimeStamp(type: Int): String {
         return when (type) {
-            DATE_TIME -> SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.KOREA).format(System.currentTimeMillis())
+            DATE_TIME -> System.currentTimeMillis().toString()
             TIME -> SimpleDateFormat("HH:mm", Locale.KOREA).format(System.currentTimeMillis())
             else -> throw RuntimeException("get time error")
         }
