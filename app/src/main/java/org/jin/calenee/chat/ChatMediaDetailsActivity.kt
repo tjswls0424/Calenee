bb775d6eac4067ebcbb3294f84401bc089805d40
tjswls0424 <dltjswls0424@gmail.com>
2022-10-16 08:04:01 +0900

    [add] save image file

--- app/src/main/java/org/jin/calenee/chat/ChatMediaDetailsActivity.kt
+++ app/src/main/java/org/jin/calenee/chat/ChatMediaDetailsActivity.kt
@@ -1,15 +1,22 @@
 package org.jin.calenee.chat
 
+import android.content.ContentValues
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.core.graphics.drawable.toBitmap
+import com.google.android.material.snackbar.Snackbar
 import org.jin.calenee.R
 import org.jin.calenee.databinding.ActivityChatMediaDetailsBinding
+import java.io.File
+import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -93,6 +100,43 @@ class ChatMediaDetailsActivity : AppCompatActivity() {
 
     }
 
+    private fun saveImageFile() {
+        try {
+            val fileName = imageData.timeInMillis.toString()
+            var fos: OutputStream? = null
+
+            val contentValues = ContentValues().apply {
+                put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+                put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+                put(
+                    MediaStore.MediaColumns.RELATIVE_PATH,
+                    Environment.DIRECTORY_PICTURES + File.separator + "Calenee"
+                )
+            }
+
+            contentResolver.apply {
+                val imageUri =
+                    insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues).apply {
+                        val id = this?.path?.split("/") ?: listOf()
+                        Log.d("img_test/path", this?.path.toString())
+                    }
+
+                fos = imageUri?.let {
+                    openOutputStream(it)
+                }
+            }
+
+            fos?.use {
+                imageData.bitmap?.compress(Bitmap.CompressFormat.JPEG, 90, it)
+                Snackbar.make(binding.root, "사진이 저장되었습니다", Snackbar.LENGTH_SHORT).show()
+            }
+            fos?.flush()
+            fos?.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.chat_top_menu, menu)
         return true
@@ -105,12 +149,13 @@ class ChatMediaDetailsActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.chat_image_share -> {
-                Log.d("menu_test", "chat_image_share")
+            R.id.chat_image_download -> {
+                saveImageFile()
+                Log.d("menu_test", "chat_image_download")
                 return true
             }
-            R.id.chat_image_download -> {
-                Log.d("menu_test", "chat_image_download")
+            R.id.chat_image_share -> {
+                Log.d("menu_test", "chat_image_share")
                 return true
             }
         }
