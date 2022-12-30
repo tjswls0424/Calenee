981d3b3fddd09bd410a1550cb008129ec1fa7ec5
tjswls0424 <dltjswls0424@gmail.com>
2022-12-30 17:36:40 +0900

    [fix] reset message data to hide today message(text) on main screen if text position is not set (position == 0)

--- app/src/main/java/org/jin/calenee/home/TodayMessagePositionActivity.kt
+++ app/src/main/java/org/jin/calenee/home/TodayMessagePositionActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import org.jin.calenee.App
 import org.jin.calenee.databinding.ActivityTodayMessagePositionBinding
 
 class TodayMessagePositionActivity : AppCompatActivity() {
@@ -31,6 +34,42 @@ class TodayMessagePositionActivity : AppCompatActivity() {
         }
         binding.saveBtn.setOnClickListener {
             val intent = if (messagePosition == 0) {
+                // "설정안함"
+                // reset message data
+                Firebase.firestore.collection("coupleInfo")
+                    .document(App.userPrefs.getString("couple_chat_id"))
+                    .apply {
+                        when (coupleInfo.position) {
+                            1 -> {
+                                update("user${coupleInfo.position}Message", coupleInfo.user1Message)
+                                App.userPrefs.updateTodayMessageInfo(
+                                    coupleInfo.position,
+                                    coupleInfo.user1Message,
+                                    0,
+                                    0,
+                                    0,
+                                    0
+                                )
+                            }
+                            2 -> {
+                                update("user${coupleInfo.position}Message", coupleInfo.user2Message)
+                                App.userPrefs.updateTodayMessageInfo(
+                                    coupleInfo.position,
+                                    coupleInfo.user2Message,
+                                    0,
+                                    0,
+                                    0,
+                                    0
+                                )
+                            }
+                        }
+
+                        update("user${coupleInfo.position}MessagePosition", 0)
+                        update("user${coupleInfo.position}MessageAlignment", 0)
+                        update("user${coupleInfo.position}MessageSize", 0)
+                        update("user${coupleInfo.position}MessageColor", 0)
+                    }
+
                 Toast.makeText(this, "저장 되었습니다", Toast.LENGTH_SHORT).show()
                 Intent(this, EditCoupleInfoActivity::class.java).apply {
                     addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
